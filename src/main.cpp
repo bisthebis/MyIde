e25b29f92f97c278f25067b45c5d68404d0689c0
Boris Martin <bism25@hotmail.com>
2016-11-03 22:29:12 +0100

    Launching Process

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,18 @@
 #include <QtCore>
+#include <QCoreApplication>
+#include <QProcess>
 
 
 int main(int argc, char** argv)
 {
 	QCoreApplication app(argc, argv);
 
-    QTimer::singleShot(1000, [](){qDebug() << "Leaving..."; qApp->quit();});
+    QProcess* process = new QProcess(&app);
+    process->start("chromium");
+
+    /* Weird cast because of overloaded signal */
+    QObject::connect(process, static_cast<void(QProcess::*)(int, QProcess::ExitStatus)>(&QProcess::finished),
+                     &app, &QCoreApplication::quit);
 	
 	return app.exec();
 
